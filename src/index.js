472eb27ceed62ef51a137a859ce1ab10ae4e7de3
simba999 <simba1512@yahoo.com>
2018-01-30 23:33:27 +0000

    Add npm scripts and disable playground in production

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ const Query = require('./resolvers/Query');
 
 const resolvers = {
     Query
-}
+};
+
+const isDevelopment = 'production' !== process.env.NODE_ENV;
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
-    context: req => ({...args})
+    context: req => ({...req})
 });
 
-server.start(() => console.log('Server is running on localhost:4000'));
+const options = {
+    port: process.env.PORT || 4000,
+    playground: isDevelopment ? '/' : false
+};
+
+server.start(options , ({port}) => console.log(`Server is running on http://localhost:${port}`));
