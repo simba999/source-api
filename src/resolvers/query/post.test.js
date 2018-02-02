9d349ef6882251fdfee4b954dbb05da6512a1f64
simba999 <simba1512@yahoo.com>
2018-02-02 13:15:30 +0000

    Add tests for post resolver

---
+++ src/resolvers/query/post.test.js
@@ -0,0 +1,40 @@
+const post = require("./post");
+const processMarkdown = require("../../lib/process-markdown");
+
+const testMarkdownContent = `
+---
+path: "/blog/hello-world"
+date: "2018-01-26"
+title: "Hello World"
+---
+
+Hello world, this is the initial post!`;
+
+let readFile;
+let args;
+let context;
+
+beforeEach(() => {
+    args = { id: "hello-world" };
+    readFile = jest.fn();
+    readFile.mockImplementation(() => Promise.resolve(testMarkdownContent));
+    context = { processMarkdown, fs: { readFile } };
+});
+
+test("it calls readFile once", () => {
+    const result = post({}, args, context);
+    expect(readFile.mock.calls.length).toBe(1);
+});
+
+test("it calls readFile with the correct args", () => {
+    const result = post({}, args, context);
+    expect(readFile.mock.calls[0][0]).toBe("content/hello-world.md");
+});
+
+test('it should return the correct data', () => {
+    const result = post({}, args, context).then(data => {
+        expect(data.content).toBe('<p>Hello world, this is the initial post!</p>\n');
+        expect(data.id).toBe('/blog/hello-world');
+        expect(data.title).toBe('Hello World');
+    });
+})
